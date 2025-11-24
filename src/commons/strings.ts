32239bd37404fbfc9d556b42911d8191a1dea5cb
Phuc Le <hphuc5453@gmail.com>
2025-11-24 16:46:52 +0700

    Setup transform interceptor for base response

---
+++ src/commons/strings.ts
@@ -0,0 +1,8 @@
+export const MESSAGES = {
+    SUCCESS: 'Success',
+    ERROR: 'Error',
+
+    USER_NOT_FOUND: 'User not found',
+    EMAIL_EXISTED: 'Email already existed',
+    INVALID_TOKEN: 'Invalid token',
+}
