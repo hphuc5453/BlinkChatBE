0d5e24833e59b2c4d5b03a0558439d1dcf0f5f5e
Phuc Le <hphuc5453@gmail.com>
2025-11-27 09:19:31 +0700

    Validate for channel message param

--- src/commons/strings.ts
+++ src/commons/strings.ts
@@ -11,3 +11,8 @@ export const AUTH_MESSAGES = {
 export const GLOBAL_EXCEPTION = {
     INTERAL_SERVER_ERROR: 'Internal server error'
 }
+
+export const SENDBIRD_EXCEPTION = {
+    USER_CREATION_FAILED: 'Failed to create user in Sendbird',
+    CHANNEL_TYPE_INVALID: 'Invalid channel type'
+}
--- src/modules/messages/message.service.ts
+++ src/modules/messages/message.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { SendbirdService } from "../sendbird/sendbird.service";
 import { Message } from "./interfaces/message.interface";
+import { ChannelType } from "../sendbird/commons/channel.type";
+import { SENDBIRD_EXCEPTION } from "src/commons/strings";
 
 @Injectable()
 export class MessageService {
     constructor(private readonly sendbirdService: SendbirdService) { }
 
     async getChannelMessages(channelType: string, channelUrl: string, messageTs: number): Promise<Array<Message>> {
+
+        if (!messageTs) {
+            messageTs = Date.now();
+        }
+
+        if(ChannelType.GROUP !== channelType && ChannelType.OPEN !== channelType) {
+            throw new BadRequestException(SENDBIRD_EXCEPTION.CHANNEL_TYPE_INVALID);
+        }
+
         const messages = await this.sendbirdService.getChannelMessages(channelType, channelUrl, messageTs);
         return messages.map((e: any) => ({
             id: e.message_id,
